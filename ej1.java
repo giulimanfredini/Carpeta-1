0713be96813978edcc5e9cc06ac1534047d9d981
giulimanfredini <gmanfredini@colegiodesanjose.edu.ar>
2024-04-25 14:22:44 -0300

    hola

---
+++ ej1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ferreyra_manfredini4b;
+
+/**
+ *
+ * @author PC02
+ */
+public class ej1 {
+    
+public static void main(String[] args)
+{
+ int a = 5 ;
+ int b = 8 ;
+ int suma ;
+ suma = a+b;
+System.out.println("La suma es:");
+System.out.println(suma);
+}
+ 
+
+}
